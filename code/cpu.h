1f7d6c9c5383cb9494bb5cddafad094fe5cfe727
= <=>
2025-05-31 21:37:40 -0400

    added STA/X/Y opcode constants

--- code/cpu.h
+++ code/cpu.h
@@ -1,6 +1,5 @@
-#ifndef DEBUG
+#ifndef CPU_H
 
-#endif // !DEBUG
 #include <stdint.h>
 
 typedef unsigned char Byte;
@@ -14,6 +13,7 @@ typedef struct Memory Memory;
 
 /* Instructions */
 
+/* Read instructions */
 static constexpr Byte INS_LDA_IM = 0xA9;
 static constexpr Byte INS_LDA_ZP = 0xA5;
 static constexpr Byte INS_LDA_ZPX = 0xB5;
@@ -32,7 +32,26 @@ static constexpr Byte INS_LDY_ZP = 0xA4;
 static constexpr Byte INS_LDY_ZPX = 0xB4;
 static constexpr Byte INS_LDY_ABS = 0xAC;
 static constexpr Byte INS_LDY_ABX = 0xBC;
+
+/* Write Instructions */
+static constexpr Byte INS_STA_ZP = 0x85;
+static constexpr Byte INS_STA_ZPX = 0x95;
+static constexpr Byte INS_STA_ABS = 0x8D;
+static constexpr Byte INS_STA_ABX = 0x9D;
+static constexpr Byte INS_STA_ABY = 0x99;
+static constexpr Byte INS_STA_IDX = 0x81;
+static constexpr Byte INS_STA_IDY = 0x91;
+static constexpr Byte INS_STX_ZP = 0x86;
+static constexpr Byte INS_STX_ZPY = 0x96;
+static constexpr Byte INS_STX_ABS = 0x8E;
+static constexpr Byte INS_STY_ZP = 0x84;
+static constexpr Byte INS_STY_ZPX = 0x94;
+static constexpr Byte INS_STY_ABS = 0x8C;
+
 /* Program Flow */
 static constexpr Byte INS_JSR = 0x20;
 static constexpr Byte INS_JMP_ABS = 0x4C;
 static constexpr Byte INS_JMP_IND = 0x6C;
+
+#define CPU_H
+#endif // !CPU_H
